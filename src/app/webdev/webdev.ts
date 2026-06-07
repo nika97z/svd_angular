7812218f6e7dd39d8aa2a7d17fee79de0afb2709
nika97z <zirakishvili3@gmail.com>
2026-06-07 20:04:56 +0400

    webdev: update webdev component files

--- src/app/webdev/webdev.ts
+++ src/app/webdev/webdev.ts
@@ -1,17 +1,37 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Atom } from '../atom/atom';
+import { Footer } from '../footer/footer';
 
 
 @Component({
   selector: 'app-webdev',
-  imports: [RouterLink, Atom],
+  imports: [RouterLink, Atom, Footer],
   templateUrl: './webdev.html',
   styleUrl: './webdev.scss',
 })
 export class Webdev {
   isMenuOpen = signal(false);
+  expandedServices = signal<Record<number, boolean>>({});
+
   toggleMenu(): void {
     this.isMenuOpen.update(v => !v);
+  }
+
+  toggleService(index: number): void {
+    this.expandedServices.update(services => {
+      const isCurrentOpen = services[index];
+      const newServices: Record<number, boolean> = {};
+      
+      if (!isCurrentOpen) {
+        newServices[index] = true;
+      }
+      
+      return newServices;
+    });
+  }
+
+  isServiceExpanded(index: number): boolean {
+    return this.expandedServices()[index] || false;
   }
 }
