e952908b5dc58a29e28f4403482372f8e5a2caa3
nika97z <zirakishvili3@gmail.com>
2026-07-27 05:40:28 +0400

    chatposition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Main } from './main/main';
-import { Ss } from './ss/ss';
 import { Webdev } from './webdev/webdev';
 import { Automatization } from './automatization/automatization';
 import { Chb } from './chb/chb';
@@ -10,7 +9,6 @@ import { Udesing } from './udesing/udesing';
 
 export const routes: Routes = [
     { path: '', component: Main },
-    { path: 'ss', component: Ss },
     { path: 'web-development', component: Webdev},
     { path: 'ai-automatization', component: Automatization},
     { path: 'chatbot', component: Chb},
